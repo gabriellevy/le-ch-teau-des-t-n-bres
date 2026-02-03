0987bde617d3a02278e27cd4bf492d9aaf9e9bec
gabriellevy <mdeharbe@hotmail.com>
2026-02-03 22:43:28 +0100

    page d'accueil

--- src/compo/CoverPage.tsx
+++ src/compo/CoverPage.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import couverture from "../images/couverture.jpg"; // Assure-toi que le fichier existe
 
 type CoverPageProps = {
-    onStart: () => void; // Fonction appelée au clic
+    onStart: () => void;
 };
 
 export const CoverPage: React.FC<CoverPageProps> = ({ onStart }) => {
     return (
-        <div
-            className="cover-page"
-            onClick={onStart}
-            style={{
-                backgroundImage: `url(${couverture})`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}
-        >
+        <div className="cover-page-container">
+            <img
+                src={couverture}
+                alt="Le Château des Ténèbres"
+                className="cover-image"
+                onClick={onStart}
+            />
             <div className="cover-overlay">
                 <p className="cover-text">Cliquez pour commencer l'aventure...</p>
             </div>
